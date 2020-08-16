1b4e1f2b852a7635c838d54d289db27647016bf8
wrs <493453108@qq.com>
2020-08-17 00:50:27 +0800

    基于geth crypto库和EIP 55的ValidatePrivateKeyAndAddress方法完善

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/ethereum/go-ethereum v1.9.19
 	github.com/labstack/echo/v4 v4.1.16
+	github.com/labstack/gommon v0.3.0
 	github.com/pkg/errors v0.9.1
 	github.com/satori/go.uuid v1.2.0
 )
--- wallet/usecase/wallet_ucase_offline.go
+++ wallet/usecase/wallet_ucase_offline.go
@@ -4,7 +4,6 @@ import (
 	"encoding/hex"
 	"fmt"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
 	uuid "github.com/satori/go.uuid"
 	"my-echo-server-project/domain"
@@ -12,7 +11,6 @@ import (
 )
 
 type walletUsecaseOffline struct {
-
 }
 
 func (w walletUsecaseOffline) ExtractAddressFromSignedMessage(message string, signedMessage string) (address string, err error) {
@@ -41,18 +39,27 @@ func (w walletUsecaseOffline) GenerateRandomMessage() (message string, err error
 }
 
 func (w walletUsecaseOffline) ValidatePrivateKeyAndAddress(message string, signedMessage string, expectedAddress string) (res bool, err error) {
-	bytes, err := hex.DecodeString(signedMessage[2:])
+	sig, err := hex.DecodeString(signedMessage[2:])
 	if err != nil {
 		fmt.Print(err.Error())
 		return false, err
 	}
-	byteAddress, err := EcRecover([]byte(message), bytes)
+	// see go-ethereum/internal/ethapi/api.go EcRecover
+	if len(sig) != 65 || (sig[64] != 27 && sig[64] != 28) {
+		return false, nil
+	}
+	sig[64] -= 27
+	// TODO maybe last puzzle, why there is only one possible pubkey from elliptic curve?
+	pubKey, err := crypto.SigToPub(signHash([]byte(message)), sig)
+	if err != nil {
+		return false,err
+	}
+	recoveredAddress := crypto.PubkeyToAddress(*pubKey)
 	if err != nil {
 		fmt.Print(err.Error())
 		return false, err
 	}
-	address := byteAddress.String()
-	return strings.ToLower(address) == expectedAddress, nil
+	return recoveredAddress == common.HexToAddress(expectedAddress), nil
 }
 
 // signHash is a helper function that calculates a hash for the given message that can be
@@ -65,57 +72,4 @@ func (w walletUsecaseOffline) ValidatePrivateKeyAndAddress(message string, signe
 func signHash(data []byte) []byte {
 	msg := fmt.Sprintf("\x19Ethereum Signed Message:\n%d%s", len(data), data)
 	return crypto.Keccak256([]byte(msg))
-}
-
-//// Sign calculates an Ethereum ECDSA signature for:
-//// keccack256("\x19Ethereum Signed Message:\n" + len(message) + message))
-////
-//// Note, the produced signature conforms to the secp256k1 curve R, S and V values,
-//// where the V value will be 27 or 28 for legacy reasons.
-////
-//// The key used to calculate the signature is decrypted with the given password.
-////
-//// https://github.com/ethereum/go-ethereum/wiki/Management-APIs#personal_sign
-//func (s *PrivateAccountAPI) Sign(ctx context.Context, data hexutil.Bytes, addr common.Address, passwd string) (hexutil.Bytes, error) {
-//	// Look up the wallet containing the requested signer
-//	account := accounts.Account{Address: addr}
-//
-//	wallet, err := s.b.AccountManager().Find(account)
-//	if err != nil {
-//		return nil, err
-//	}
-//	// Assemble sign the data with the wallet
-//	signature, err := wallet.SignHashWithPassphrase(account, passwd, signHash(data))
-//	if err != nil {
-//		log.Warn("Failed data sign attempt", "address", addr, "err", err)
-//		return nil, err
-//	}
-//	signature[64] += 27 // Transform V from 0/1 to 27/28 according to the yellow paper
-//	return signature, nil
-//}
-
-// EcRecover returns the address for the account that was used to create the signature.
-// Note, this function is compatible with eth_sign and personal_sign. As such it recovers
-// the address of:
-// hash = keccak256("\x19Ethereum Signed Message:\n"${message length}${message})
-// addr = ecrecover(hash, signature)
-//
-// Note, the signature must conform to the secp256k1 curve R, S and V values, where
-// the V value must be 27 or 28 for legacy reasons.
-//
-// https://github.com/ethereum/go-ethereum/wiki/Management-APIs#personal_ecRecover
-func  EcRecover( data, sig hexutil.Bytes) (common.Address, error) {
-	if len(sig) != 65 {
-		return common.Address{}, fmt.Errorf("signature must be 65 bytes long")
-	}
-	if sig[64] != 27 && sig[64] != 28 {
-		return common.Address{}, fmt.Errorf("invalid Ethereum signature (V is not 27 or 28)")
-	}
-	sig[64] -= 27 // Transform yellow paper V from 27/28 to 0/1
-
-	rpk, err := crypto.SigToPub(signHash(data), sig)
-	if err != nil {
-		return common.Address{}, err
-	}
-	return crypto.PubkeyToAddress(*rpk), nil
 }
